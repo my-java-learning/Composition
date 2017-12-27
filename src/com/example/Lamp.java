6f9389887e0a6cbff9f0722e2ebcdcbad9bd2311
Qsior <lukaszbojes@gmail.com>
2017-12-27 13:19:04 +0100

    Added bedroom composition example

---
+++ src/com/example/Lamp.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class Lamp {
+    private String style;
+    private boolean battery;
+    private int globRating;
+
+    public Lamp(String style, boolean battery, int globRating) {
+        this.style = style;
+        this.battery = battery;
+        this.globRating = globRating;
+    }
+
+    public void turnOn(){
+        System.out.println("Lamp -> turning on");
+    }
+}
--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -9,5 +9,21 @@ public class Main {
         Motherboard motherboard = new Motherboard("BJ-200" , "Asus" , 4 , 6 , "v2.44");
         PC pc = new PC(theCase, monitor, motherboard);
         pc.powerUp();
+
+        Wall wall1 = new Wall("West");
+        Wall wall2 = new Wall("South");
+        Wall wall3 = new Wall("North");
+        Wall wall4 = new Wall("East");
+
+        Ceiling ceiling = new Ceiling(12, 55);
+
+        Bed bed = new Bed("Modelr", 4,3,2,1);
+
+        Lamp lamp = new Lamp("Classic", false, 75);
+
+        Bedroom bedroom = new Bedroom("Tims" , wall1, wall2, wall3, wall4 , ceiling, bed , lamp);
+
+        bedroom.makeBed();
+        bedroom.getLamp().turnOn();
     }
 }
