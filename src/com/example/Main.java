bc2a2b4902beda504f36d1bec308292a171fcae0
Qsior <lukaszbojes@gmail.com>
2017-12-27 02:19:15 +0100

    Added composition example, pc object is being made of case, monitor and motherboard object

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,6 +3,13 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-
+        Dimensions dimensions = new Dimensions(20,20,5);
+        Case theCase = new Case("220B", "Dell" , "240" , dimensions);
+        Monitor monitor = new Monitor("27inch Beast" , "Acer" , 27 , new Resolution(2540,1440));
+        Motherboard motherboard = new Motherboard("BJ-200" , "Asus" , 4 , 6 , "v2.44");
+        PC pc = new PC(theCase, monitor, motherboard);
+        pc.getMonitor().drawPixelAt(1500,1200,"red");
+        pc.getMotherboard().loadProgram("Windows 1.0");
+        pc.getTheCase().pressPowerButton();
     }
 }
